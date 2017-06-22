709af12a77dfe86cbb1b218b8a5b0cb0b980455c
胡邦茂 <1414430109@qq.com>
2017-06-22 18:33:51 +0800

    修改提交

--- page/static/lib/import-css.js
+++
@@ -1,3 +0,0 @@
-document.write("<link type='text/css' rel='stylesheet' href= 'http://oprzeq6of.bkt.clouddn.com/bootstrap-theme.css'/>");
-document.write("<link type='text/css' rel='stylesheet' href= 'http://oprzeq6of.bkt.clouddn.com/jquery-ui.min.css'/>");
-
--- page/static/lib/import-js.js
+++
@@ -1,3 +0,0 @@
-document.write("<script type='text/javascript' src='http://oprzeq6of.bkt.clouddn.com/jquery-3.2.1.min.js'></script>");
-document.write("<script type='text/javascript' src='http://oprzeq6of.bkt.clouddn.com/jquery-ui.min.js'></script>");
-document.write("<script type='text/javascript' src='http://oprzeq6of.bkt.clouddn.com/bootstrap.js'></script>");
